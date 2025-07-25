3f68642245099da6fccd84234726810486eb2db4
missing_nia <ardenprado@yahoo.com>
2025-07-25 14:54:49 -0700

    Embed comments under currently active video
    Testing comment fetching per video

--- database/seeders/CommentSeeder.php
+++ database/seeders/CommentSeeder.php
@@ -13,6 +13,6 @@ class CommentSeeder extends Seeder
      */
     public function run(): void
     {
-        Comment::factory(20)->create();
+        Comment::factory(100)->create();
     }
 }
--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -19,41 +19,56 @@ export default function Index({videos}) {
                             className="block mx-auto mt-5"
                             src={`https://www.youtube.com/embed/${video.video_id}`}
                         />
+
+                        {/*Not sure if hardcoding the prev next is the best way but
+                        using normal paginate when i only need a prev/next feels kinda wasteful*/}
+                        {/*Navigation Arrows*/}
+                        <div>
+                            {videos.prev_page_url ?
+                                <Link
+                                    key="&laquo"
+                                    href={videos.prev_page_url} 
+                                    dangerouslySetInnerHTML={{__html: "&laquo"}}
+                                    className= "p-1 mx-1 text-5xl text-white"
+                                    preserveScroll
+                                />
+                            :
+                            <span 
+                                dangerouslySetInnerHTML={{__html: "&laquo"}}
+                                className="p-1 mx-1 text-stone-500 text-5xl"
+                            />
+                            }
+                            {videos.next_page_url ?
+                                <Link
+                                    key="&raquo"
+                                    href={videos.next_page_url} 
+                                    dangerouslySetInnerHTML={{__html: "&raquo"}}
+                                    className= "p-1 mx-1 text-5xl text-white"
+                                    preserveScroll
+                                />
+                            :
+                            <span 
+                                dangerouslySetInnerHTML={{__html: "&raquo"}}
+                                className="p-1 mx-1 text-stone-500 text-5xl"
+                            />
+                            }
+                        </div>
+
+                        {/*Comments*/}
+                        <div>
+                            {video.comments.map(comment =>(
+                                <div key={comment.id} className="text-stone-800 border-b">
+                                    <h1 className="text-stone-500">
+                                        {comment.user.username} at {new Date(comment.created_at).toLocaleTimeString()}
+                                    </h1>
+                                    <span className="text-white text-base">
+                                        {comment.body}
+                                    </span>
+                                </div>
+                            ))}
+                        </div>
                     </div>
                 ))}
-
-                {/*Not sure if hardcoding the prev next is the best way but
-                   using normal paginate when i only need a prev/next feels kinda wasteful*/}
-                <div>
-                    {videos.prev_page_url ?
-                        <Link
-                            key="&laquo"
-                            href={videos.prev_page_url} 
-                            dangerouslySetInnerHTML={{__html: "&laquo"}}
-                            className= "p-1 mx-1 text-5xl text-white"
-                            preserveScroll
-                        />
-                    :
-                    <span 
-                        dangerouslySetInnerHTML={{__html: "&laquo"}}
-                        className="p-1 mx-1 text-stone-500 text-5xl"
-                    />
-                    }
-                    {videos.next_page_url ?
-                        <Link
-                            key="&raquo"
-                            href={videos.next_page_url} 
-                            dangerouslySetInnerHTML={{__html: "&raquo"}}
-                            className= "p-1 mx-1 text-5xl text-white"
-                            preserveScroll
-                        />
-                    :
-                    <span 
-                        dangerouslySetInnerHTML={{__html: "&raquo"}}
-                        className="p-1 mx-1 text-stone-500 text-5xl"
-                    />
-                    }
-                </div>
             </div>
         </>
     );
