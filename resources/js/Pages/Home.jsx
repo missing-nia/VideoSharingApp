39eb981c75e1d56a326ad12b33b8b6741f683775
missing_nia <ardenprado@yahoo.com>
2025-07-26 00:43:20 -0700

    Title and "upload" timestamp on video listings

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,17 +1,31 @@
+import timeSince from "../Components/TimeSince";
+
 export default function Home({videos}) {
+    var data = videos.data;
     console.log(videos)
     return(
         <>
             <div className="min-h-screen bg-stone-900 text-center">
                 <div className="grid md:grid-cols-[repeat(3,minmax(200px,1fr))] sm:grid-cols-[repeat(2,minmax(200px,1fr))] gap-4 px-25 py-10">
-                    {videos.data.map(video => (
-                        <div key={video.id} className="aspect-16/9">
+                    {data.map(video => (
+                        <div key={video.id}>
                             <img 
-                                className="rounded-lg"
+                                className="aspect-16/9 rounded-lg"
                                 src={`https://img.youtube.com/vi/${video.video_id}/maxresdefault.jpg`}
                             />
+                            <p className="text-white">
+                                {video.title}
+                                <h1 className="text-stone-500 text-sm">
+                                    {video.user.username}
+                                </h1>
+                                <h1 className="text-stone-500 text-sm">
+                                    {timeSince(new Date(video.created_at))} ago
+                                </h1>
+                            </p>
                         </div>
                     ))}
+                </div>
+                <div>
                 </div>
             </div>
 </>
