288c092ec506d35bf724aff4096f7a6504eb09e7
missing_nia <ardenprado@yahoo.com>
2025-07-26 13:41:02 -0700

    Adjust show view scaling and positioning

--- resources/js/Pages/Show.jsx
+++ resources/js/Pages/Show.jsx
@@ -4,9 +4,9 @@ export default function Show({video}) {
     return(
         <>
             <div className="min-h-screen bg-stone-900">
-                <div className="ml-50 text-left">
+                <div className="md:pl-25 ml-10 md:ml-25 lg:ml-35 mr-10 lg:mr-10 xl:mr-5 text-left">
                     <iframe
-                        className="aspect-video rounded-lg w-[70%]"
+                        className="aspect-video rounded-lg w-[100%] lg:w-[75%]"
                         allow="fullscreen"
                         src={`https://www.youtube.com/embed/${video.video_id}`}
                     />
@@ -18,8 +18,8 @@ export default function Show({video}) {
                         Shared by {video.user.username}
                     </span>
                     <div className="flex justify mt-5">
-                        <div className="text-left box-content w-4xl bg-stone-800 rounded-md p-3 text-white">
-                            <div className="h-8/10 w-8/10">
+                        <div className="text-left box-content w-[100%] lg:w-[75%] bg-stone-800 rounded-lg text-white">
+                            <div className="h-8/10 w-6/10 p-3">
                                 <span>
                                     {new Date(video.created_at).toLocaleDateString('en-US', { day: 'numeric', month: 'long', year: 'numeric' })} 
                                 </span>
@@ -32,7 +32,7 @@ export default function Show({video}) {
                     </div>
                     {/*Comments*/}
                     <div className="flex justify py-8">
-                        <div className="text-left box-content">
+                        <div className="text-left box-content w-[100%] lg:w-[75%]">
                             <h1 className="text-white text-3xl font-bold">Comments</h1>
                             {video.comments.map(comment =>(
                                 <div key={comment.id} className="text-stone-800 border-b py-5">
