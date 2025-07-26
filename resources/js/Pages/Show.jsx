43fe9889ebdf68b9c9064d5f722426445776a58c
missing_nia <ardenprado@yahoo.com>
2025-07-26 05:20:17 -0700

    Show page formatting improvements and description box

--- resources/js/Pages/Show.jsx
+++ resources/js/Pages/Show.jsx
@@ -14,20 +14,38 @@ export default function Show({video}) {
                 </span>
                 <br></br>
                 <span className="text-stone-500">
-                    {new Date(video.created_at).toLocaleTimeString()} by {video.user.username}
+                    Shared by {video.user.username}
                 </span>
-                {/*Comments*/}
-                <div>
-                    {video.comments.map(comment =>(
-                        <div key={comment.id} className="text-stone-800 border-b">
-                            <h1 className="text-stone-500">
-                                {comment.user.username} at {new Date(comment.created_at).toLocaleTimeString()}
-                            </h1>
-                            <span className="text-white text-base">
-                                {comment.body}
+                <div className="flex justify-center mt-5">
+                    <div className="text-left box-content w-4xl bg-stone-800 rounded-md p-3 text-white">
+                        <div className="h-8/10 w-8/10">
+                            <span>
+                                {new Date(video.created_at).toLocaleDateString('en-US', { day: 'numeric', month: 'long', year: 'numeric' })} 
+                            </span>
+                            <br/>
+                            <span>
+                                {video.description}
                             </span>
                         </div>
-                    ))}
+                    </div>
+                </div>
+                {/*Comments*/}
+                <div className="flex justify-center p-8">
+                    <div className="text-left box-content">
+                        <h1 className="text-white text-3xl font-bold">
+                            Comments
+                        </h1>
+                        {video.comments.map(comment =>(
+                            <div key={comment.id} className="text-stone-800 border-b py-5">
+                                <h1 className="text-stone-500">
+                                    {comment.user.username} at {new Date(comment.created_at).toLocaleTimeString()}
+                                </h1>
+                                <span className="text-white text-base">
+                                    {comment.body}
+                                </span>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
         </>
