86835d251cb95a0753ffd36c2d77629fd636aea5
missing_nia <ardenprado@yahoo.com>
2025-07-26 02:35:57 -0700

    Add single interval of time formatting adjustment for timeSince

--- resources/js/Components/TimeSince.jsx
+++ resources/js/Components/TimeSince.jsx
@@ -3,25 +3,49 @@ export default function timeSince(date) {
   var seconds = Math.floor((new Date() - date) / 1000);
 
   var interval = seconds / 31536000;
+  var format = "";
+  var intervalAmount = 0;
 
   if (interval > 1) {
-    return Math.floor(interval) + " years";
+    intervalAmount = Math.floor(interval)
+    format = " years";
+    return intervalAmount + adjustFormatting(intervalAmount, format);
   }
   interval = seconds / 2592000;
   if (interval > 1) {
-    return Math.floor(interval) + " months";
+    intervalAmount = Math.floor(interval);
+    format = " months";
+    return intervalAmount + adjustFormatting(intervalAmount, format);
   }
   interval = seconds / 86400;
   if (interval > 1) {
-    return Math.floor(interval) + " days";
+    intervalAmount = Math.floor(interval);
+    format = " days";
+    return intervalAmount + adjustFormatting(intervalAmount, format);
   }
   interval = seconds / 3600;
   if (interval > 1) {
-    return Math.floor(interval) + " hours";
+    intervalAmount = Math.floor(interval);
+    format = " hours";
+    return intervalAmount + adjustFormatting(intervalAmount, format);
   }
   interval = seconds / 60;
   if (interval > 1) {
-    return Math.floor(interval) + " minutes";
+    intervalAmount = Math.floor(interval);
+    format = " minutes";
+    return intervalAmount + adjustFormatting(intervalAmount, format);
   }
-  return Math.floor(seconds) + " seconds";
+  intervalAmount = Math.floor(seconds); 
+  format = " seconds";
+  return intervalAmount + adjustFormatting(intervalAmount, format);
+}
+
+// Simple function for removing trailin s from time format if the quantity is 1
+function adjustFormatting(intervalAmount, format) {
+    if (intervalAmount == 1)
+    {
+        // Remove the s 
+        return format.substring(0, format.length - 1);
+    }
+    return format
 }
