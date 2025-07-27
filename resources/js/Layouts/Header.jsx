a85bb2bc2e8765bf9e5bc6c245732ce8e313a715
missing_nia <ardenprado@yahoo.com>
2025-07-27 01:32:11 -0700

    fix calling text style multiple times in a single class

--- resources/js/Layouts/Header.jsx
+++ resources/js/Layouts/Header.jsx
@@ -5,7 +5,7 @@ export default function Header({children}) {
         <>
             <header className="bg-stone-900 shadow-md h-15 nav-header">
                 <nav className="py-3.5">
-                    <Link href='/' className='text-white rounded-md px-4 text-xl font-bold text-3xl'>VideoShare</Link>
+                    <Link href='/' className='text-white rounded-md px-4 text-xl font-bold'>VideoShare</Link>
                 </nav>
             </header>
 
