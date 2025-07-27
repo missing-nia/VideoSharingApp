9feff2b827e4cd872741def940876cb3b312c089
missing_nia <ardenprado@yahoo.com>
2025-07-27 04:13:25 -0700

    Login, Logout, Register Link styles

--- resources/js/Layouts/Header.jsx
+++ resources/js/Layouts/Header.jsx
@@ -1,11 +1,28 @@
-import { Link } from "@inertiajs/react";
+import { Link, usePage } from "@inertiajs/react";
 
 export default function Header({children}) {
+    const { auth } = usePage().props;
+    console.log(auth);
+
     return (
         <>
             <header className="bg-stone-900 shadow-md h-15 nav-header">
-                <nav className="py-3.5">
-                    <Link href='/' className='text-white rounded-md px-4 text-xl font-bold'>VideoShare</Link>
+                <nav>
+                    <div className="flex h-14 items-center justify-between">
+                        <div>
+                            <Link href='/' className='text-white rounded-md px-4 text-xl font-bold'>VideoShare</Link>
+                        </div>
+                        {!auth.user ? (
+                            <div className="flex items-center text-white gap-4 px-4 font-semibold">
+                                <Link href='/register'>Register</Link>
+                                <Link href=''>Login</Link>
+                            </div>
+                        ) : (
+                            <div className="flex items-center text-white gap-4 px-4 font-semibold">
+                                <Link href=''>Logout</Link>
+                            </div>
+                        )}
+                    </div>
                 </nav>
             </header>
 
